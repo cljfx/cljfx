f189d95732d97f45f9b6cb3742e285c3aa834f39
Vladislav Protsenko <vladislav.protsenko@arbetsformedlingen.se>
2020-05-20 13:28:50 +0200

    Add minor improvements to web-view example

--- examples/e34_web_browser.clj
+++ examples/e34_web_browser.clj
@@ -2,7 +2,9 @@
   (:require [cljfx.api :as fx]
             [cljfx.lifecycle :as lifecycle]
             [cljfx.mutator :as mutator]
-            [cljfx.prop :as prop]))
+            [cljfx.prop :as prop]
+            [clojure.string :as str])
+  (:import [javafx.scene.web WebView]))
 
 ;; A simple web browser
 ;; ====================
@@ -11,10 +13,10 @@
 ;; clicked. See the README.md and e21-extension-lifecycle for some additional examples of this.
 
 (def web-view-with-ext-props
-  (cljfx.api/make-ext-with-props
-    {:on-location-changed (prop/make (mutator/property-change-listener
-                                             #(.locationProperty (.getEngine %)))
-                                           cljfx.lifecycle/change-listener)}))
+  (fx/make-ext-with-props
+   {:on-location-changed (prop/make (mutator/property-change-listener
+                                      #(.locationProperty (.getEngine ^WebView %)))
+                                    lifecycle/change-listener)}))
 ;; A well known URL
 (def home-url "https://www.google.com")
 
@@ -26,58 +28,58 @@
                    ::current-url home-url}))
 
 (defn top-pane [{:keys [state]}]
-  {:fx/type  :h-box
-   :spacing  5
-   :children [{:fx/type   :button
-               :text      "Home"
+  {:fx/type :h-box
+   :spacing 5
+   :children [{:fx/type :button
+               :text "Home"
                :on-action {:event/type ::home}}
-              {:fx/type         :text-field
-               :h-box/hgrow     :always
-               :text            (::partial-url state)
-               :on-text-changed {:event/type ::url-change}
-               :on-action       {:event/type ::url-complete}}]})
+              {:fx/type :text-field
+               :h-box/hgrow :always
+               :text (::partial-url state)
+               :on-text-changed {:event/type ::url-typed}
+               :on-action {:event/type ::url-submitted}}]})
 
 ;; The web-pane function returns the extended web-view that has the additional property :on-location-changed installed.
 (defn web-pane [{:keys [state]}]
   {:fx/type web-view-with-ext-props
-   :desc    {:fx/type     :web-view
-             :pref-height 1000
-             :pref-width  1500
-             :url         (::current-url state)}
-   :props   {:on-location-changed {:event/type ::url-change}}})
+   :desc {:fx/type :web-view
+          :pref-height 1000
+          :pref-width 1500
+          :url (::current-url state)}
+   :props {:on-location-changed {:event/type ::url-changed}}})
 
-  (defn body-pane [{:keys [state]}]
-    {:fx/type  :v-box
-     :padding  10
-     :spacing  10
-     :children [{:fx/type top-pane
-                 :state   state}
-                {:fx/type web-pane
-                 :state   state}]})
+(defn body-pane [{:keys [state]}]
+  {:fx/type :v-box
+   :padding 10
+   :spacing 10
+   :children [{:fx/type top-pane
+               :state state}
+              {:fx/type web-pane
+               :state state}]})
 
 (defn root [state]
-    {:fx/type :stage
-     :showing true
-     :title   "Simple web browser"
-     :scene   {:fx/type :scene
-               :root    {:fx/type body-pane :state state}}})
+  {:fx/type :stage
+   :showing true
+   :title "Simple web browser"
+   :scene {:fx/type :scene
+           :root {:fx/type body-pane :state state}}})
 
-  (defn normalise [s]
-    (let [omitted "http://"]
-      (if (clojure.string/starts-with? s omitted)
-        s
-        (str omitted s))))
+(defn normalise [s]
+  (if (str/starts-with? s "http")
+    s
+    (str "http://" s)))
 
-  (defn event-handler [event]
-    (case (:event/type event)
-      ::home (reset! *state {::partial-url home-url
-                             ::current-url home-url})
-      ::url-change (do (prn (:fx/event event)) (swap! *state assoc ::partial-url (:fx/event event)))
-      ::url-complete (swap! *state assoc ::current-url (-> @*state ::partial-url normalise))))
+(defn event-handler [{:keys [fx/event event/type]}]
+  (case type
+    ::home (reset! *state {::partial-url home-url
+                           ::current-url home-url})
+    ::url-typed (swap! *state assoc ::partial-url event)
+    ::url-submitted (swap! *state #(assoc % ::current-url (-> % ::partial-url normalise)))
+    ::url-changed (swap! *state assoc ::current-url event ::partial-url event)))
 
-  (def renderer
-    (fx/create-renderer
-      :middleware (fx/wrap-map-desc assoc :fx/type root)
-      :opts {:fx.opt/map-event-handler event-handler}))
+(def renderer
+  (fx/create-renderer
+   :middleware (fx/wrap-map-desc assoc :fx/type root)
+   :opts {:fx.opt/map-event-handler event-handler}))
 
-  (fx/mount-renderer *state renderer)
+(fx/mount-renderer *state renderer)
