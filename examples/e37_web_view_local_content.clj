beb3ca3623eb1504ddec8c77ab3b71d113224381
manutter51 <manutter51@gmail.com>
2020-09-25 07:30:30 -0400

    Add snippet showing local content in a web view
    Copied from @vlaaad's example in the #cljfx channel on the clojurians Slack.
    
    I don't understand everything in this code, I'm just making a PR so it can be preserved in
    the Examples when it scrolls out of view in Slack.

---
+++ examples/e37_web_view_local_content.clj
@@ -0,0 +1,22 @@
+(ns e37-web-view-local-content
+  (:require '[cljfx.api :as fx]
+            '[cljfx.prop :as fx.prop]
+            '[cljfx.mutator :as fx.mutator]
+            '[cljfx.lifecycle :as fx.lifecycle]))
+
+;; Short snippet demonstrating how to display local content in a WebView
+
+(def ext-with-html
+  (fx/make-ext-with-props
+    {:html (fx.prop/make
+             (fx.mutator/setter #(.loadContent (.getEngine ^javafx.scene.web.WebView %1) %2))
+             fx.lifecycle/scalar)}))
+
+(fx/on-fx-thread
+  (fx/create-component
+    {:fx/type :stage
+     :showing true
+     :scene {:fx/type :scene
+             :root {:fx/type ext-with-html
+                    :props {:html "<h1>hello html!</h1>"}
+                    :desc {:fx/type :web-view}}}}))
