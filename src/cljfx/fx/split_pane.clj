d5c4e818e61aed13289760655583572b24e6468e
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2021-06-27 01:13:33 -0400

    Add :split-pane/resizable-with-parent extra prop

--- src/cljfx/fx/split_pane.clj
+++ src/cljfx/fx/split_pane.clj
@@ -3,7 +3,9 @@
   (:require [cljfx.composite :as composite]
             [cljfx.lifecycle :as lifecycle]
             [cljfx.coerce :as coerce]
-            [cljfx.fx.control :as fx.control])
+            [cljfx.fx.control :as fx.control]
+            [cljfx.prop :as prop]
+            [cljfx.mutator :as mutator])
   (:import [javafx.scene.control SplitPane]
            [javafx.geometry Orientation]))
 
@@ -20,7 +22,12 @@
       :divider-positions [:setter lifecycle/scalar
                           :coerce #(into-array Double/TYPE (map double %))
                           :default []]
-      :items [:list lifecycle/dynamics]
+      :items [:list (-> lifecycle/dynamic
+                        (lifecycle/wrap-extra-props
+                          {:split-pane/resizable-with-parent (prop/make
+                                                               (mutator/setter #(SplitPane/setResizableWithParent %1 %2))
+                                                               lifecycle/scalar)})
+                           lifecycle/wrap-many)]
       :orientation [:setter lifecycle/scalar :coerce (coerce/enum Orientation)
                     :default :horizontal])))
 
