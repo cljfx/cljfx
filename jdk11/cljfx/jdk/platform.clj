5a7c7102ab05a4fb935b2c5cb066d8547ca433c4
vlaaad <v.proczenko@gmail.com>
2021-09-22 20:22:05 +0200

    Silence javafx warnings by default

--- jdk11/cljfx/jdk/platform.clj
+++ jdk11/cljfx/jdk/platform.clj
@@ -1,8 +1,14 @@
 (ns cljfx.jdk.platform
-  (:import [javafx.application Platform]))
+  (:import [javafx.application Platform]
+           [java.util.logging LogManager]
+           [java.io ByteArrayInputStream]))
 
 (defn initialize []
   (try
+    (when-not (Boolean/getBoolean "cljfx.log-javafx-warnings")
+      (.readConfiguration
+        (LogManager/getLogManager)
+        (ByteArrayInputStream. (.getBytes "\"javafx\".level=SEVERE" "UTF-8"))))
     (Platform/startup #(Platform/setImplicitExit false))
     :cljfx.platform/initialized
     (catch IllegalStateException _
